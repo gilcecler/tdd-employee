4d0c1e291c5b660c83a10b9d45ef77302d7dbc2c
Gilcecler Carneiro <gilcecler@gmail.com>
2024-08-10 10:33:03 -0300

    primeiro testdepartment - implementado

--- src/main/java/com/devsuperior/demo/repositories/DepartmentRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/DepartmentRepository.java
@@ -3,5 +3,5 @@ package com.devsuperior.demo.repositories;
 import com.devsuperior.demo.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartamentRepository extends JpaRepository<Department, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
